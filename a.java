0a12b1d0bc559f579f412a209e3f5ec9fc47137d
770prince775 <bhardwajprince2635@gmail.com>
2025-11-17 11:20:53 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,14 @@
+public class a {
+    static{   // this is the static method in java // 
+        System.out.println("hello"); 
+    }
+    public static void main(String[] args) {
+        System.out.println("i am in main mehtod ");
+    }
+    // static method java me memory management ke kaam aata h // 
+    static {
+        System.out.println("this is the second static method ");
+        // aab main method se phele ye print hoga // 
+            
+    }
+}
