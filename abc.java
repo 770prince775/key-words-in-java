cbbdd9f3d05030d4c08e20f040c05be9b0e0b8e1
770prince775 <bhardwajprince2635@gmail.com>
2025-11-17 13:44:35 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,8 @@
+public class abc { // final keyword in java // 
+    public static void main(String[] args) {
+    final    int a = 10; // aab ye error show kar dega // 
+        a = a+20; // aab iski value change nahi hogi // 
+        System.out.println(a); 
+    }
+    
+} 
