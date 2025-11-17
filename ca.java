31545cddae518a4a65d3a99f84af4bf15dfddff5
770prince775 <bhardwajprince2635@gmail.com>
2025-11-17 12:04:14 +0530

    Add files via upload

---
+++ ca.java
@@ -0,0 +1,10 @@
+public class ca { // static keyword ki non access modifier // 
+      static int a = 10; // ye instance variable h  lekin static lagane ke baad ye static baan jagya // 
+     void m1(){ 
+        static int b = 20;  // ye ek local variable h or ye error show kar dega // 
+         // local varibel ke saath static keybwird ka use nahi kar sakte h // 
+    }
+     public static void main(String[] args) {
+        
+     }
+}
