9854d6cdd352971bbe0ef5b8da3ecb6423ceae65
770prince775 <bhardwajprince2635@gmail.com>
2025-11-17 11:39:48 +0530

    Add files via upload

---
+++ aa.java
@@ -0,0 +1,11 @@
+public class aa {
+    // native methods vo mehtods hote h jo jinka code kisi dusri language ke andar implement kia jata h // 
+    static int a;
+    static{
+        System.out.println(a = 10); 
+    }
+    public static void main(String[] args) {
+        System.out.println("I am in main method");
+    }
+    
+}
