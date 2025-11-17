51bf51b1b461514dea1a410550d79b1636d2cd13
770prince775 <bhardwajprince2635@gmail.com>
2025-11-17 12:14:34 +0530

    Add files via upload

---
+++ aac.java
@@ -0,0 +1,10 @@
+public class aac {
+  static int a = 10;
+    
+    
+}
+class demo{
+    public static void main(String[] args) {
+        System.out.println(aac.a); // jab variable static hoga thab hi access kar skate h  dusri class ke vbariable ko // 
+    }
+}
